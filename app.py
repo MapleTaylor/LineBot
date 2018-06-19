0eaa8a37e61c897ef96612e907b4e64ca209b03d
MapleTaylor <f0966066981@gmail.com>
2018-06-19 18:18:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    text = event.message.text + '\n' #message from user
+    text = event.message.text #message from user
 
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text)) #reply the same message from user
     
