93b17407a3c64d09093b5e2acd00032b8cdd23cc
MapleTaylor <f0966066981@gmail.com>
2018-06-29 00:54:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+from googleTranslater import *
+from moodDeterminer import *
+
 app = Flask(__name__)
 line_bot_api = LineBotApi('quz2dQQwCibQaIaWOJ7Y2uz6Uh486G+WHjLAFvuhYN0EHur2EtKMstVo+Y0niIjNhV9adAg/LDK7pY1T2ZRuBeH+Rga1DljktGG+2/fNmdw9FX0kyN6HWiV0zUyj3Vkhk7EcWdGgSjCmTcc/c9e3EwdB04t89/1O/w1cDnyilFU=') # Your Channel Access Token
 handler = WebhookHandler('456d1cdec1fb86bc17c82d3c0c3941c6') # Your Channel Secret
@@ -27,21 +30,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    url = "https://tw.beanfun.com/maplestory/Bullentin_alpha/_BullentinDefault.aspx"
-    request = requests.get(url)
-    content = request.content
-    soup = BeautifulSoup(content, "html.parser")
-    hoarding = soup.select(".maple01")
-    # text = event.message.text # message from user
-    text = []
-    for i in hoarding[1:]:
-	    h = i.find("a").get("href")
-	    if not "https" in h:
-		    text.append("https://tw.beanfun.com/%s" % h)
-	    else:
-		    text.append(h)
-    for i in text:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=i)) # reply the same message from user
+    translater = GoogleTranslater()
+    determiner = MoodDeterminer()
+    translater.sendText()
+    receive1 = translater.getText()
+    determiner.sendText(receive1)
+    text = determiner.getText()
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text)) # reply the same message from user
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=os.environ['PORT'])
